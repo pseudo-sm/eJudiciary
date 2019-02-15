eaae5932d12cb448a9f14e997c9aaf87c3e12e46
pseudo-sm <saswathcommand@gmail.com>
2019-02-15 14:53:07 +0530

    Index added URLs py Generated

--- ejapp/urls.py
+++ ejapp/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
 
-    url(r'',include('ejapp.urls')),
+    url(r'',views.index,name="index")
 
 ]
